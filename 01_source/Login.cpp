30e4c0caff31771fb9f36841cd713a2dde88ec9e
Nara <aurora.raa252@gmail.com>
2025-11-30 20:34:42 +0700

    Refactor Login class and update verifyLogin method

--- 01_source/Login.cpp
+++ 01_source/Login.cpp
@@ -2,27 +2,25 @@
 //  Login.cpp
 //  Implementation of the Class Login
 //  Created on:      30-Nov-2025 7:58:30 PM
-//  Original author: moog
+//  Original author: nara
 ///////////////////////////////////////////////////////////
 
 #include "Login.h"
-
+#include <iostream>
 
 Login::Login(){
-
+	emailLogin = "";
+	passLogin = "";
 }
-
-
 
 Login::~Login(){
 
 }
 
-
-
-
-
 bool Login::verifyLogin(){
-
-	return false;
+	cout << "Enter your email: ";
+	cin >> emailLogin;
+	cout << "Enter your password: ";
+	getline(cin, passLogin);
+	return true;
 }
--- 01_source/Login.h
+++ 01_source/Login.h
@@ -2,24 +2,23 @@
 //  Login.h
 //  Implementation of the Class Login
 //  Created on:      30-Nov-2025 7:58:30 PM
-//  Original author: moog
+//  Original author: nara
 ///////////////////////////////////////////////////////////
 
-#if !defined(EA_B4BC7310_9AE7_4798_BEE0_BFB3E3242D15__INCLUDED_)
-#define EA_B4BC7310_9AE7_4798_BEE0_BFB3E3242D15__INCLUDED_
-
+#ifndef LOGIN_H
+#define LOGIN_H
+#include <string>
+using namespace std;
 class Login
 {
 
 public:
 	Login();
 	virtual ~Login();
-
 	bool verifyLogin();
 
 private:
 	string emailLogin;
 	string passLogin;
-
 };
-#endif // !defined(EA_B4BC7310_9AE7_4798_BEE0_BFB3E3242D15__INCLUDED_)
+#endif // LOGIN_H

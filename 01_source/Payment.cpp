5553f9f7eb4f5b83715896e3c88241bd42d089c2
Nara <aurora.raa252@gmail.com>
2025-11-30 20:43:01 +0700

    Fix Payment class initialization and totalCost logic

--- 01_source/Payment.cpp
+++ 01_source/Payment.cpp
@@ -2,27 +2,20 @@
 //  Payment.cpp
 //  Implementation of the Class Payment
 //  Created on:      30-Nov-2025 7:58:32 PM
-//  Original author: moog
+//  Original author: nara
 ///////////////////////////////////////////////////////////
 
 #include "Payment.h"
 
-
 Payment::Payment(){
-
+	orderCost = 0.0;
+	orderQuantity = 0;
 }
-
-
 
 Payment::~Payment(){
 
 }
 
-
-
-
-
 double Payment::totalCost(){
-
-	return 0;
+	return orderCost * orderQuantity;
 }
--- 01_source/Payment.h
+++ 01_source/Payment.h
@@ -2,24 +2,21 @@
 //  Payment.h
 //  Implementation of the Class Payment
 //  Created on:      30-Nov-2025 7:58:32 PM
-//  Original author: moog
+//  Original author: nara
 ///////////////////////////////////////////////////////////
 
-#if !defined(EA_9A238272_BAEB_410a_BE98_83C05703DA01__INCLUDED_)
-#define EA_9A238272_BAEB_410a_BE98_83C05703DA01__INCLUDED_
-
+#ifndef PAYMENT_H
+#define PAYMENT_H
 class Payment
 {
 
 public:
 	Payment();
 	virtual ~Payment();
-
 	double totalCost();
 
 private:
 	double orderCost;
 	int orderQuantity;
-
 };
-#endif // !defined(EA_9A238272_BAEB_410a_BE98_83C05703DA01__INCLUDED_)
+#endif // PAYMENT_H

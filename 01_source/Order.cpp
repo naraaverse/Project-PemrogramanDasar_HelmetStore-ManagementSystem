ced8a6bcdf2c7989d372928753b98d82c0f61f01
Nara <aurora.raa252@gmail.com>
2025-11-30 20:39:15 +0700

    Refactor Order class and update helmetOrder method

--- 01_source/Order.cpp
+++ 01_source/Order.cpp
@@ -2,26 +2,34 @@
 //  Order.cpp
 //  Implementation of the Class Order
 //  Created on:      30-Nov-2025 7:58:31 PM
-//  Original author: moog
+//  Original author: nara
 ///////////////////////////////////////////////////////////
 
 #include "Order.h"
-
+#include <iostream>
 
 Order::Order(){
-
+    customerName = "";
+    helmetBrand = "";
+    helmetColor = "";
+    helmetType = "";
+    paymentMethod = "";
 }
-
-
 
 Order::~Order(){
 
 }
 
-
-
-
-
 void Order::helmetOrder(){
-
+    cout << "Detail Helmet Order" << endl;
+    cout << "Customer Name: ";
+    getline(cin, customerName);
+    cout << "Helmet Brand: ";
+    getline(cin, helmetBrand);
+    cout << "Helmet Color: ";
+    cin >> helmetColor;
+    cout << "Helmet Type: ";
+    cin >> helmetType;
+    cout << "Payment Method: ";
+    cin >> paymentMethod;
 }
--- 01_source/Order.h
+++ 01_source/Order.h
@@ -2,24 +2,22 @@
 //  Order.h
 //  Implementation of the Class Order
 //  Created on:      30-Nov-2025 7:58:31 PM
-//  Original author: moog
+//  Original author: nara
 ///////////////////////////////////////////////////////////
 
-#if !defined(EA_84692220_7AA5_45ef_8320_C664E3BC9174__INCLUDED_)
-#define EA_84692220_7AA5_45ef_8320_C664E3BC9174__INCLUDED_
+#ifndef ORDER_H
+#define ORDER_H
 
-#include "OrderInfo.h"
 #include "Payment.h"
-
+#include <string>
+using namespace std;
 class Order
 {
 
 public:
 	Order();
 	virtual ~Order();
-	OrderInfo *m_OrderInfo;
 	Payment *m_Payment;
-
 	void helmetOrder();
 
 private:
@@ -28,6 +26,5 @@ private:
 	string helmetColor;
 	string helmetType;
 	string paymentMethod;
-
 };
-#endif // !defined(EA_84692220_7AA5_45ef_8320_C664E3BC9174__INCLUDED_)
+#endif // ORDER_H

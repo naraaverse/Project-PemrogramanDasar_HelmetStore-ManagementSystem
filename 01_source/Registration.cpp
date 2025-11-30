1788034e23cf8df4a3e52b86b25f0a8ae144a8ca
Nara <aurora.raa252@gmail.com>
2025-11-30 20:46:06 +0700

    Refactor Registration class and update verifyData method

--- 01_source/Registration.cpp
+++ 01_source/Registration.cpp
@@ -2,26 +2,26 @@
 //  Registration.cpp
 //  Implementation of the Class Registration
 //  Created on:      30-Nov-2025 7:58:30 PM
-//  Original author: moog
+//  Original author: nara
 ///////////////////////////////////////////////////////////
 
 #include "Registration.h"
-
+#include <iostream>
 
 Registration::Registration(){
-
+    emailUser = "";
+    passUser = "";
 }
-
-
 
 Registration::~Registration(){
 
 }
 
-
-
-
-
 void Registration::verifyData(){
-
+    cout << "Register your account" << endl;
+    cout << "Enter your email: ";
+    cin >> emailUser;
+    cout << "Enter your password: ";
+    cin >> passUser;
+    cout << "Registration successful!" << endl;
 }
--- 01_source/Registration.h
+++ 01_source/Registration.h
@@ -2,24 +2,23 @@
 //  Registration.h
 //  Implementation of the Class Registration
 //  Created on:      30-Nov-2025 7:58:29 PM
-//  Original author: moog
+//  Original author: nara
 ///////////////////////////////////////////////////////////
 
-#if !defined(EA_DA35F5F9_8576_48a3_9A81_7BBC51D8ECEE__INCLUDED_)
-#define EA_DA35F5F9_8576_48a3_9A81_7BBC51D8ECEE__INCLUDED_
-
+#ifndef REGISTRATION_H
+#define REGISTRATION_H
+#include <string>
+using namespace std;
 class Registration
 {
 
 public:
 	Registration();
 	virtual ~Registration();
-
 	void verifyData();
 
 private:
 	string emailUser;
 	string passUser;
-
 };
-#endif // !defined(EA_DA35F5F9_8576_48a3_9A81_7BBC51D8ECEE__INCLUDED_)
+#endif // REGISTRATION_H
